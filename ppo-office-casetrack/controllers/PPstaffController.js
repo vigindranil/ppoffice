ed750175fb42ca606caa99f70df5cd2cd8448b62
montril-user <mon47236147@gmail.com>
2024-12-06 14:42:51 +0530

    show pp staff #4

--- ppo-office-casetrack/controllers/PPstaffController.js
+++ ppo-office-casetrack/controllers/PPstaffController.js
@@ -54,6 +54,31 @@ class UserController {
       });
     });
   }
+
+  static showppstaff(req, res) {
+    const query = 'CALL sp_getPPstaff()';  // Replace with your stored procedure name
+
+    db.query(query, (err, results) => {
+      if (err) {
+        console.error('Error executing stored procedure:', err);
+        return res.status(500).json({
+          status: 1,
+          message: 'Error retrieving data from the database',
+          data: []
+        });
+      }
+
+      // Assuming your stored procedure returns data in results[0]
+      const response = {
+        status: 0,
+        message: 'data found',
+        data: results[0] // The data returned from the stored procedure
+      };
+
+      // Send the formatted JSON response
+      res.json(response);
+    });
+  }
 }
 
 module.exports = UserController;
