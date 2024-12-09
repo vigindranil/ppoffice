054ae226e4ea84822be7fdef8f39c9ce72f90983
danupam822 <danupam822@gmail.com>
2024-12-09 16:10:20 +0530

    remove extra code

--- WBTenantVerificationService/lang/en/MsgUploadValidation.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'SuccessStatusUpdate' => 'Status Updated Successfully',
-    'FailureStatusUpdate' => 'Failed to update Status!',
-    'NullApprovalStatus' => 'Approval Status is Required!',
-];
--- WBTenantVerificationService/resources/views/dailylog.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('app')
-
-@section('content')
-<div class="container">
-    <h1>Daily Log of API</h1>
-    <pre>{{ $logContents }}</pre>
-</div>
-@endsection
