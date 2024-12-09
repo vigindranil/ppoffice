87ccd395b988a03e01e8dfc9718bab221f3121be
tanaygangully <tanaybhusangangully@gmail.com>
2024-12-09 13:13:01 +0530

    added by tanay:- solved cors errors.

--- ppo-office-casetrack/server.js
+++ ppo-office-casetrack/server.js
@@ -1,22 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-//const port = 8000;  // You can change the port if you need
-
-// Middleware to parse JSON bodies
-app.use(express.json());
-
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;  // Use the port from .env or default to 3000
 
+// Enable CORS for all routes
+app.use(cors());
+
+// Middleware to parse JSON bodies
+app.use(express.json());
 
 const routes = require('./routes/routes');
 
 app.use('/', routes);
 
-
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
