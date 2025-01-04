5ec3e56b5b3cb9885e00b949d414bce7962e1e3a
montril-user <mon47236147@gmail.com>
2025-01-04 22:27:19 +0530

    slove db connection #44

--- ppo-office-casetrack/config/db.js
+++ ppo-office-casetrack/config/db.js
@@ -7,6 +7,9 @@ const db = mysql.createPool({
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
     connectTimeout: 10000, // Timeout in milliseconds
 });
 
