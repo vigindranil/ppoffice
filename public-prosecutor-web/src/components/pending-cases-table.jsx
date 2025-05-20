9373e62ad833d544c7414284711a33e02429d12c
MillenR-Vyoma <millen.r@vyomainnovusglobal.com>
2025-05-20 18:45:06 +0530

    update

--- public-prosecutor-web/src/components/pending-cases-table.jsx
+++ public-prosecutor-web/src/components/pending-cases-table.jsx
@@ -58,30 +58,49 @@ export default function CaseTable() {
 
   const showallCaseBetweenRange = async (start, end) => {
 
-   try {
-         setLoading(true)
-         const token = sessionStorage.getItem('token');
-         const response = await postRequest("showallCaseBetweenRange", {
-           startDate: formatDate(start),
-           endDate: formatDate(end),
-           isAssign: 0,
-           EntryUserID: user.AuthorityUserID
-         })
-         if (response.status === 0) {
-           setAllCases(response.data)
-         } else {
-           throw new Error(response.message || 'Failed to fetch data')
-         }
-       } catch (err) {
-         setError(err.message)
-       } finally {
-         setLoading(false)
-       }
+    try {
+      setLoading(true)
+      const token = sessionStorage.getItem('token');
+      const response = await postRequest("showallCaseBetweenRange", {
+        startDate: formatDate(start),
+        endDate: formatDate(end),
+        isAssign: 0,
+        EntryUserID: user.AuthorityUserID
+      })
+      if (response.status === 0) {
+        setAllCases(response.data)
+      } else {
+        throw new Error(response.message || 'Failed to fetch data')
+      }
+    } catch (err) {
+      setError(err.message)
+    } finally {
+      setLoading(false)
+    }
   }
+
+  // useEffect(() => {
+  //   const decoded_user = JSON.parse(decrypt(userDetails));
+  //   setUser(decoded_user);
+  // }, [userDetails]);
 
   useEffect(() => {
     const decoded_user = JSON.parse(decrypt(userDetails));
     setUser(decoded_user);
+
+    const storedFrom = sessionStorage.getItem("pending-case-table-start-date");
+    const storedTo = sessionStorage.getItem("pending-case-table-end-date");
+
+    const today = new Date();
+    const todayFormatted = formatDate(today);
+
+    setFromDate(storedFrom || todayFormatted);
+    setToDate(storedTo || todayFormatted);
+
+    showallCaseBetweenRange(
+      formatDate(storedFrom || todayFormatted),
+      formatDate(storedTo || todayFormatted)
+    );
   }, [userDetails]);
 
   useEffect(() => {
@@ -117,177 +136,183 @@ export default function CaseTable() {
             onConfirm={handleConfirm}
           />
           <div className="container mx-auto px-6 py-8 w-full">
-          <div className="mt-12 bg-white dark:bg-gray-800 rounded-xl shadow-md overflow-hidden px-4">
-            <div className="bg-gradient-to-r from-cyan-600 to-violet-600 px-6 py-3">
-              <h2 className="text-2xl font-bold text-white">Pending Cases</h2>
-            </div>
-            <div className="container mx-auto py-10">
+            <div className="mt-12 bg-white dark:bg-gray-800 rounded-xl shadow-md overflow-hidden px-4">
+              <div className="bg-gradient-to-r from-cyan-600 to-violet-600 px-6 py-3">
+                <h2 className="text-2xl font-bold text-white">Pending Cases</h2>
+              </div>
+              <div className="container mx-auto py-10">
 
-              <div className='flex gap-4 mb-3'>
+                <div className='flex gap-4 mb-3'>
 
-              <div className="max-w-xs">
-              <Label className="font-bold" htmlFor="fromDate">
-                From Date
-              </Label>
-              <Input
-                icon={Calendar}
-                id="fromDate"
-                name="fromDate"
-                type="date"
-                value={fromDate ? formatDate(fromDate) : ""}
-                onChange={(e) => setFromDate(e.target.value)}
-                placeholder="From (Date Range)"
-              />
-              </div>
+                  <div className="max-w-xs">
+                    <Label className="font-bold" htmlFor="fromDate">
+                      From Date
+                    </Label>
+                    <Input
+                      icon={Calendar}
+                      id="fromDate"
+                      name="fromDate"
+                      type="date"
+                      value={fromDate ? formatDate(fromDate) : ""}
+                      onChange={(e) => setFromDate(e.target.value)}
+                      placeholder="From (Date Range)"
+                    />
+                  </div>
 
-              <div className="max-w-xs">
-              <Label className="font-bold" htmlFor="toDate">
-                To Date
-              </Label>
-              <Input
-                icon={Calendar}
-                id="toDate"
-                name="toDate"
-                type="date"
-                value={toDate ? formatDate(toDate) : ""}
-                onChange={(e) => setToDate(e.target.value)}
-                placeholder="To (Date Range)"
-              />
-              </div>
-              <div className="flex items-end">
-                <Button
-                  className="ml-2 bg-blue-400 hover:bg-blue-600"
-                  onClick={() => showallCaseBetweenRange(formatDate(fromDate), formatDate(toDate))}
-                >{loading ? 'Loading...' : 'Get Cases'}</Button>
-              </div>
-              </div>
-              <div className='w-100 h-[1px] bg-slate-100 my-4'></div>
-              <div className="flex justify-between items-center mb-4">
-                <div className="relative">
-                  <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
-                  <Input
-                    type="text"
-                    placeholder="Search Cases..."
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                    className="pl-8"
-                  />
+                  <div className="max-w-xs">
+                    <Label className="font-bold" htmlFor="toDate">
+                      To Date
+                    </Label>
+                    <Input
+                      icon={Calendar}
+                      id="toDate"
+                      name="toDate"
+                      type="date"
+                      value={toDate ? formatDate(toDate) : ""}
+                      onChange={(e) => setToDate(e.target.value)}
+                      placeholder="To (Date Range)"
+                    />
+                  </div>
+                  <div className="flex items-end">
+                    <Button
+                      className="ml-2 bg-blue-400 hover:bg-blue-600"
+                      onClick={() => {
+                        sessionStorage.setItem("pending-case-table-start-date", fromDate);
+                        sessionStorage.setItem("pending-case-table-end-date", toDate);
+                        showallCaseBetweenRange(formatDate(fromDate), formatDate(toDate));
+                      }}
+                    >
+                      {loading ? 'Loading...' : 'Get Cases'}
+                    </Button>
+                  </div>
                 </div>
-                <div>
-                  <span className="mr-2 text-xs">Total number of records: {filteredData.length}</span>
+                <div className='w-100 h-[1px] bg-slate-100 my-4'></div>
+                <div className="flex justify-between items-center mb-4">
+                  <div className="relative">
+                    <Search className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
+                    <Input
+                      type="text"
+                      placeholder="Search Cases..."
+                      value={searchTerm}
+                      onChange={(e) => setSearchTerm(e.target.value)}
+                      className="pl-8"
+                    />
+                  </div>
+                  <div>
+                    <span className="mr-2 text-xs">Total number of records: {filteredData.length}</span>
+                  </div>
                 </div>
-              </div>
-              <Table>
-                <TableHeader>
-                  <TableRow className="bg-slate-100">
-                    <TableHead className="font-bold">Case Date</TableHead>
-                    <TableHead className="font-bold">Case Number</TableHead>
-                    <TableHead className="font-bold">PS Name</TableHead>
-                    <TableHead className="font-bold">Case Status</TableHead>
-                    <TableHead className="font-bold">View</TableHead>
-                    <TableHead className="font-bold">Actions</TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {currentCases.map((caseItem, index) => (
-                    <TableRow key={index}>
-                      <TableCell>{formatDate(caseItem.CaseDate)}</TableCell>
-                      <TableCell>{caseItem.CaseNumber}</TableCell>
-                      <TableCell>{caseItem.PsName}</TableCell>
-                      <TableCell>{caseItem.IsAssigned ? <Badge className='bg-emerald-400'>Assigned</Badge> : <Badge className='bg-orange-300'>Pending</Badge>}</TableCell>
-                      <TableCell>
-                        <Dialog open={isCaseSelected} onOpenChange={setIsCaseSelected}>
-                          <DialogTrigger asChild>
-                            <Button
-                              variant="outline"
-                              onClick={() => {
-                                setIsCaseSelected(true)
-                                setSelectedCase(caseItem)
-                              }}
-                            >
-                              <Eye /> More Details
-                            </Button>
-                          </DialogTrigger>
-                          <DialogContent className="sm:max-w-auto">
-                            <DialogHeader>
-                              <DialogTitle>Case Details</DialogTitle>
-                            </DialogHeader>
-                            <DialogDescription>
-                              These informations are for case {selectedCase?.CaseNumber}
-                            </DialogDescription>
-                            <Card>
-                              <CardContent>
-                                {selectedCase && (
-                                  <>
-                                    <div className="space-y-2">
-                                      <p><strong>Case Date:</strong> {formatDate(selectedCase.CaseDate)}</p>
-                                      <p><strong>Case Number:</strong> {selectedCase.CaseNumber}</p>
-                                      <p><strong>SP Name:</strong> {selectedCase.SpName}</p>
-                                      <p><strong>PS Name:</strong> {selectedCase.PsName}</p>
-                                      <p><strong>Case Type:</strong> {selectedCase.CaseType}</p>
-                                      <p><strong>Case Hearing Date:</strong> {formatDate(selectedCase.CaseHearingDate)}</p>
-                                      <p>
+                <Table>
+                  <TableHeader>
+                    <TableRow className="bg-slate-100">
+                      <TableHead className="font-bold">Case Date</TableHead>
+                      <TableHead className="font-bold">Case Number</TableHead>
+                      <TableHead className="font-bold">PS Name</TableHead>
+                      <TableHead className="font-bold">Case Status</TableHead>
+                      <TableHead className="font-bold">View</TableHead>
+                      <TableHead className="font-bold">Actions</TableHead>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    {currentCases.map((caseItem, index) => (
+                      <TableRow key={index}>
+                        <TableCell>{formatDate(caseItem.CaseDate)}</TableCell>
+                        <TableCell>{caseItem.CaseNumber}</TableCell>
+                        <TableCell>{caseItem.PsName}</TableCell>
+                        <TableCell>{caseItem.IsAssigned ? <Badge className='bg-emerald-400'>Assigned</Badge> : <Badge className='bg-orange-300'>Pending</Badge>}</TableCell>
+                        <TableCell>
+                          <Dialog open={isCaseSelected} onOpenChange={setIsCaseSelected}>
+                            <DialogTrigger asChild>
+                              <Button
+                                variant="outline"
+                                onClick={() => {
+                                  setIsCaseSelected(true)
+                                  setSelectedCase(caseItem)
+                                }}
+                              >
+                                <Eye /> More Details
+                              </Button>
+                            </DialogTrigger>
+                            <DialogContent className="sm:max-w-auto">
+                              <DialogHeader>
+                                <DialogTitle>Case Details</DialogTitle>
+                              </DialogHeader>
+                              <DialogDescription>
+                                These informations are for case {selectedCase?.CaseNumber}
+                              </DialogDescription>
+                              <Card>
+                                <CardContent>
+                                  {selectedCase && (
+                                    <>
+                                      <div className="space-y-2">
+                                        <p><strong>Case Date:</strong> {formatDate(selectedCase.CaseDate)}</p>
+                                        <p><strong>Case Number:</strong> {selectedCase.CaseNumber}</p>
+                                        <p><strong>SP Name:</strong> {selectedCase.SpName}</p>
+                                        <p><strong>PS Name:</strong> {selectedCase.PsName}</p>
+                                        <p><strong>Case Type:</strong> {selectedCase.CaseType}</p>
+                                        <p><strong>Case Hearing Date:</strong> {formatDate(selectedCase.CaseHearingDate)}</p>
+                                        <p>
                                           <strong>IPC Sections:</strong>{' '}
                                           {selectedCase.ipcSections && selectedCase.ipcSections.length > 0
                                             ? selectedCase.ipcSections.map(ipc => ipc.IpcSection).filter(Boolean).join(', ')
                                             : 'None'}
-                                      </p>
-                                      {/* <p><strong>Whether SP seen the mail:</strong> {selectedCase?.SP_Status ? 'Yes' : 'No'}</p>
+                                        </p>
+                                        {/* <p><strong>Whether SP seen the mail:</strong> {selectedCase?.SP_Status ? 'Yes' : 'No'}</p>
                                       <p><strong>Whether PS seen the mail:</strong> {selectedCase?.PS_Status ? 'Yes' : 'No'}</p> */}
-                                    </div>
-                                  </>
-                                )}
-                              </CardContent>
-                            </Card>
-                          </DialogContent>
-                        </Dialog>
-                      </TableCell>
-                      <TableCell>
-                        <Button
-                          variant="outline"
-                          onClick={() => {
-                            const caseID = caseItem.CaseId;
-                            const enc_caseId = btoa(caseID);
-                            router.push(`/case-control-center/${enc_caseId}`);
-                          }}
-                        >
-                          <Edit /> Take Action
-                        </Button>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-              <div className="mt-4">
-                <Pagination>
-                  <PaginationContent>
-                    <PaginationItem>
-                      <PaginationPrevious
-                        onClick={() => paginate(Math.max(1, currentPage - 1))}
-                        className={currentPage === 1 ? 'pointer-events-none opacity-50' : ''}
-                      />
-                    </PaginationItem>
-                    {[...Array(totalPages)].map((_, index) => (
-                      <PaginationItem key={index}>
-                        <PaginationLink
-                          onClick={() => paginate(index + 1)}
-                          isActive={currentPage === index + 1}
-                        >
-                          {index + 1}
-                        </PaginationLink>
-                      </PaginationItem>
+                                      </div>
+                                    </>
+                                  )}
+                                </CardContent>
+                              </Card>
+                            </DialogContent>
+                          </Dialog>
+                        </TableCell>
+                        <TableCell>
+                          <Button
+                            variant="outline"
+                            onClick={() => {
+                              const caseID = caseItem.CaseId;
+                              const enc_caseId = btoa(caseID);
+                              router.push(`/case-control-center/${enc_caseId}`);
+                            }}
+                          >
+                            <Edit /> Take Action
+                          </Button>
+                        </TableCell>
+                      </TableRow>
                     ))}
-                    <PaginationItem>
-                      <PaginationNext
-                        onClick={() => paginate(Math.min(totalPages, currentPage + 1))}
-                        className={currentPage === totalPages ? 'pointer-events-none opacity-50' : ''}
-                      />
-                    </PaginationItem>
-                  </PaginationContent>
-                </Pagination>
+                  </TableBody>
+                </Table>
+                <div className="mt-4">
+                  <Pagination>
+                    <PaginationContent>
+                      <PaginationItem>
+                        <PaginationPrevious
+                          onClick={() => paginate(Math.max(1, currentPage - 1))}
+                          className={currentPage === 1 ? 'pointer-events-none opacity-50' : ''}
+                        />
+                      </PaginationItem>
+                      {[...Array(totalPages)].map((_, index) => (
+                        <PaginationItem key={index}>
+                          <PaginationLink
+                            onClick={() => paginate(index + 1)}
+                            isActive={currentPage === index + 1}
+                          >
+                            {index + 1}
+                          </PaginationLink>
+                        </PaginationItem>
+                      ))}
+                      <PaginationItem>
+                        <PaginationNext
+                          onClick={() => paginate(Math.min(totalPages, currentPage + 1))}
+                          className={currentPage === totalPages ? 'pointer-events-none opacity-50' : ''}
+                        />
+                      </PaginationItem>
+                    </PaginationContent>
+                  </Pagination>
+                </div>
               </div>
             </div>
-          </div>
           </div>
         </main>
       </div>
